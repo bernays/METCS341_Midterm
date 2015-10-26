949af175dcfca3c06bb0a8e206c637b42a672a29
Ben Bernays <bernays@bens-macbook-pro.local>
2015-10-26 19:44:39 -0400

    Initial Commit

---
+++ CS341_Midterm/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CS341_Midterm
+//
+//  Created by Ben Bernays on 10/26/15.
+//  Copyright © 2015 Ben Bernays. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
